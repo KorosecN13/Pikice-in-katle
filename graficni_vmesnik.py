f5c5afe627964ba198864fd1f9be08ba576cd8f4
TatianaSusnik <tatianasusnik@yahoo.com.ar>
2016-03-16 14:05:44 +0100

    dodaj spreminjanje igralcev, dokončaj igro za dva igralca

--- graficni_vmesnik.py
+++ graficni_vmesnik.py
@@ -10,7 +10,7 @@ class Vmesnik():
     def __init__(self, master):
 
         # Ozadje
-        master.configure(background='pink')
+        master.configure(background='gold3')
 
         # Glavni meni
         menu = Menu(master)
@@ -33,9 +33,12 @@ class Vmesnik():
         self.ime_igralec1 = StringVar(master, value='Igralec 1')
         self.ime_igralec2 = StringVar(master, value='Igralec 2')
 
+        self.barva1 = 'light blue'
+        self.barva2 = 'orange'
+
         # Kaj moramo povedati programu
-        polje_ime_igralec1 = Entry(master, width=10, textvariable=self.ime_igralec1)
-        polje_ime_igralec2 = Entry(master, width=10, textvariable=self.ime_igralec2)
+        polje_ime_igralec1 = Entry(master, width=10, textvariable=self.ime_igralec1, background=self.barva1)
+        polje_ime_igralec2 = Entry(master, width=10, textvariable=self.ime_igralec2, background=self.barva2)
 
         # Umestimo jih na glavno okno
         polje_ime_igralec1.grid(row=1, column=0)
@@ -46,8 +49,8 @@ class Vmesnik():
         self.stevec_igralec2 = IntVar(master, value=0)
 
         # Oznake za stevce
-        oznaka_ime_igralec1 = Label(master, textvariable=self.stevec_igralec1)
-        oznaka_ime_igralec2 = Label(master, textvariable=self.stevec_igralec2)
+        oznaka_ime_igralec1 = Label(master, textvariable=self.stevec_igralec1, background=self.barva1)
+        oznaka_ime_igralec2 = Label(master, textvariable=self.stevec_igralec2, background=self.barva2)
 
         # Umestimo jih na glavno okno
         oznaka_ime_igralec1.grid(row=2, column=0)
@@ -65,7 +68,7 @@ class Vmesnik():
 
         for i in range(8):
             for j in range(8):
-                self.polje.create_oval(50*(i+1)-5, 50*(j+1)-5, 50*(i+1)+5, 50*(j+1)+5, fill='black')
+                self.polje.create_oval(50*(i+1)-5, 50*(j+1)-5, 50*(i+1)+5, 50*(j+1)+5, fill='gold4', width=0)
         self.igra = Igra(self)
         self.zacni_igro()
 
@@ -73,9 +76,10 @@ class Vmesnik():
     #    pass
 
     def nova_igra(self):
-        pass
+        self.zacni_igro()
 
     def zacni_igro(self):
+        self.na_potezi = self.ime_igralec1
         self.polje.bind('<Button-1>', self.klik)
 
     def klik(self, event):
@@ -83,17 +87,26 @@ class Vmesnik():
         # doloci kdo je na potezi
         self.igra.povleci_potezo(x, y)
 
-    def narisi_crto(self, k, i, j):
+    def narisi_crto(self, k, i, j, barva):
         """ narisi crto med ustrezna krogca na igralnem polju """
         if k == 'vodoravno':
-            self.polje.create_line(50*(j+1)+5, 50*(i+1), 50*(j+2)-5, 50*(i+1))
+            self.polje.create_line(50*(j+1)+5, 50*(i+1), 50*(j+2)-5, 50*(i+1), fill=barva, width=3)
         else:
-            self.polje.create_line(50*(j+1), 50*(i+1)+5, 50*(j+1), 50*(i+2)-5)
+            self.polje.create_line(50*(j+1), 50*(i+1)+5, 50*(j+1), 50*(i+2)-5, fill=barva, width=3)
 
-    def pobarvaj_kvadratek(self, j, i):
-        self.polje.create_rectangle(50*i+5, 50*j+5, 50*i+45, 50*j+45, fill='black')
+    def pobarvaj_kvadratek(self, j, i, barva):
+        self.polje.create_rectangle(50*i+5, 50*j+5, 50*i+45, 50*j+45, fill=barva, width=0)
 
-
+    def zmaga(self):
+        if self.stevec_igralec1.get() > self.stevec_igralec2.get():
+            zmagovalec = self.ime_igralec1.get()
+        else:
+            zmagovalec = self.ime_igralec2.get()
+        self.polje.create_rectangle(100, 150, 350, 300, fill='green yellow')
+        self.polje_id1 = self.polje.create_text(225,200,font=("Purisa", 20))
+        self.polje.itemconfig(self.polje_id1 ,text='Zmagal je')
+        self.polje_id1 = self.polje.create_text(225,250,font=("Purisa", 20))
+        self.polje.itemconfig(self.polje_id1 ,text='{0}'.format(zmagovalec))
 
 
 root = Tk()
--- igra.py
+++ igra.py
@@ -6,6 +6,12 @@ class Igra():
         self.navpicne = [[False for i in range(8)] for j in range(7)]
         self.vmesnik = vmesnik
 
+    def sprememba_igralca(self):
+        if self.vmesnik.na_potezi == self.vmesnik.ime_igralec1:
+            self.vmesnik.na_potezi = self.vmesnik.ime_igralec2
+        else: self.vmesnik.na_potezi = self.vmesnik.ime_igralec1
+
+
     def doloci_crto(self, x, y):
         """ doloci crto, ki je najblizja koordinatam miske """
         lok_x, lok_y = x % 50, y % 50
@@ -38,10 +44,13 @@ class Igra():
 
 
     def povleci_potezo(self, x, y):
+        if self.vmesnik.na_potezi == self.vmesnik.ime_igralec1:
+            barva = self.vmesnik.barva1
+        else: barva = self.vmesnik.barva2
         """ povleci poteza, ce je ta veljavna"""
         if self.veljavna_poteza(x, y):
             k ,i, j = self.doloci_crto(x, y)
-            self.vmesnik.narisi_crto(k, i, j)
+            self.vmesnik.narisi_crto(k, i, j, barva)
             if k == "vodoravno":
                 self.vodoravne[i][j] = True
             else:
@@ -49,25 +58,56 @@ class Igra():
             self.poln_kvadratek(k, i, j)
 
     def poln_kvadratek(self, k, i, j):
+        if self.vmesnik.na_potezi == self.vmesnik.ime_igralec1:
+            barva = self.vmesnik.barva1
+            stevec = self.vmesnik.stevec_igralec1
+        else:
+            barva = self.vmesnik.barva2
+            stevec = self.vmesnik.stevec_igralec2
+        p = 0 #preveri ali je kvadratek pobarvan oz. ali je drugi igralec na potezi
         if k == "vodoravno":
             if i != 0: # zgornji
                 if self.vodoravne[i-1][j]:
                     if self.navpicne[i-1][j]:
                         if self.navpicne[i-1][j+1]:
-                            self.vmesnik.pobarvaj_kvadratek(i, j+1)
+                            self.vmesnik.pobarvaj_kvadratek(i, j+1, barva)
+                            p = 1
+                            stevec.set(stevec.get()+1)
             if i != 7: # spodnji
                 if self.vodoravne[i+1][j]:
                     if self.navpicne[i][j]:
                         if self.navpicne[i][j+1]:
-                            self.vmesnik.pobarvaj_kvadratek(i+1, j+1)
+                            self.vmesnik.pobarvaj_kvadratek(i+1, j+1, barva)
+                            p = 1
+                            stevec.set(stevec.get()+1)
         else:
             if j != 0: # levi
                 if self.navpicne[i][j-1]:
                     if self.vodoravne[i][j-1]:
                         if self.vodoravne[i+1][j-1]:
-                            self.vmesnik.pobarvaj_kvadratek(i+1, j)
+                            self.vmesnik.pobarvaj_kvadratek(i+1, j, barva)
+                            p = 1
+                            stevec.set(stevec.get()+1)
             if j != 7: # desni
                 if self.navpicne[i][j+1]:
                     if self.vodoravne[i][j]:
                         if self.vodoravne[i+1][j]:
-                            self.vmesnik.pobarvaj_kvadratek(i+1, j+1)
+                            self.vmesnik.pobarvaj_kvadratek(i+1, j+1, barva)
+                            p = 1
+                            stevec.set(stevec.get()+1)
+        if self.konec_igre():
+            self.vmesnik.zmaga()
+        if p == 0:
+            self.sprememba_igralca()
+
+    def konec_igre(self):
+        return self.vmesnik.stevec_igralec1.get() + self.vmesnik.stevec_igralec2.get() == 49
+
+#    def konec_igre(self):
+#        return all([all(self.vodoravne[i])for i in len(self.vodoravne)]) and \
+#               all([all(self.navpicne[i])for i in len(self.navpicne)])
+
+
+
+
+
